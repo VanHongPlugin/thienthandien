1f2fc65c61af1da1680448ab56b5b661bab8ad6a
VanHongPlugin <168619468+VanHongPlugin@users.noreply.github.com>
2024-05-02 19:31:11 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '!!';
+const adminId = '114425881957734818';
+const { MessageEmbed } = require('discord.js');
+const { channel } = require('diagnostics_channel');
+client.on('ready', () => {
+    client.user.setActivity('Sự kiện hunt thú I')
+    client.user.setPresence({
+        status: 'dnd',
+        game: {
+          name: 'VanHong', 
+          type: 'LISTENING'
+        }
+      });
+});
+client.once('ready', () => {
+    console.log('Bot đã sẵn sàng!');
+});
+client.on('message', async message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+    const userId = message.author.id;
+    if (command === 'show') {
+        try {
+            if (!args.length) {
+                const nomsg = new Discord.MessageEmbed()
+                .setColor('#FF00EC')
+                .setDescription(`:x: Bạn cần nhập văn bản để hiển thị`);
+                message.reply(nomsg);
+            } else {
+                const msg = new Discord.MessageEmbed()
+                .setColor('#FF00EC') 
+                .setImage('https://media.discordapp.net/attachments/1234354049177092137/1234366836427198484/hunt.png?ex=66307933&is=662f27b3&hm=4a4ab1491b079cc74434bb072802300c5139fa7aa25f71640ed6b47ff64a7c3a&=&format=webp&quality=lossless&width=366&height=206')
+                .setDescription(`${args.join(' ')}`);
+                message.channel.send(msg)
+                    .then(() => {
+                        message.delete();
+                    })
+                    .catch(err => {
+                        console.error('Lỗi khi xóa tin nhắn:', err);
+                    });
+            }
+        } catch (error) {
+            console.error('Lỗi khi xử lý lệnh show message:', error);
+            message.reply('Đã xảy ra lỗi khi xử lý lệnh show message.');
+        }
+    }
+});
+client.login('MTIzMzk5Mzc5NTE2MzM4OTk5NA.GI86c5.Emqeia73FjmTlbCSDRywenlozvrBB9cKxAl4uA')
